9535104b3ee13338d82cf30288f01fe8cb683072
Yggdrasils35 <44059071+Yggdrasils35@users.noreply.github.com>
2019-07-11 00:14:39 +0800

    Delete SpotInfo.java

--- DataClass/SpotInfo.java
+++
@@ -1,9 +0,0 @@
-public class SpotInfo{
-    private int ID;                         //景点编号
-    private String Name;                    //景点名称
-    private double Longitude;               //经度位置
-    private double Latitude;                //纬度位置
-    private double TimeLength;              //建议游玩时间
-
-
-}
