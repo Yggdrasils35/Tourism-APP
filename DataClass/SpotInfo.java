1e37795b066d34f9d053e00450c45f37ae9b0632
Yggdrasils35 <44059071+Yggdrasils35@users.noreply.github.com>
2019-07-10 12:34:30 +0800

    Update SpotInfo.java
    添加了注释

--- DataClass/SpotInfo.java
+++ DataClass/SpotInfo.java
@@ -1,9 +1,9 @@
 public class SpotInfo{
-    private int ID;
-    private String Name;
-    private double Longitude;
-    private double Latitude;
-    private double TimeLength;
+    private int ID;                         //景点编号
+    private String Name;                    //景点名称
+    private double Longitude;               //经度位置
+    private double Latitude;                //纬度位置
+    private double TimeLength;              //建议游玩时间
 
 
 }
